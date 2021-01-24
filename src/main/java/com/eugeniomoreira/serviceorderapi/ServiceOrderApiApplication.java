8b1a78959cb0053c7dfcea730621d12385ff7ad9
Eugênio Moreira <eugenio1moreira@outlook.com>
2021-01-24 13:16:33 -0300

    Initial commit

---
+++ src/main/java/com/eugeniomoreira/serviceorderapi/ServiceOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.eugeniomoreira.serviceorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOrderApiApplication.class, args);
+    }
+
+}
